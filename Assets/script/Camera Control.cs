9053de0a27fc1520cea601f81de8468a48426635
JoniGF <jonikpoooooo@gmail.com>
2023-10-07 18:00:40 -0300

    Movimiento y Animacion
    Modificación del movimiento, Flipeo, Saltos, Saltos dobles
    Animación de descanso, de correr y saltar

--- Assets/script/Camera Control.cs
+++ Assets/script/Camera Control.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class controlCamara : MonoBehaviour
+public class CameraControl : MonoBehaviour
 {
     public Transform target;
     // Start is called before the first frame update
@@ -14,6 +14,6 @@ public class controlCamara : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3 (target.position.x, target.position.y, target.position.z);
+        transform.position = new Vector3(target.position.x, transform.position.y, transform.position.z);
     }
 }
