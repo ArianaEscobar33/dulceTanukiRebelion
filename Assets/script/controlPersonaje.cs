c088cc0b42671595bff99ceb3cf55a07b7ab2a67
ArianaEscobar27 <66396056+ArianaEscobar27@users.noreply.github.com>
2023-10-07 01:04:48 -0300

    movimietno horizontal

--- Assets/script/controlPersonaje.cs
+++ Assets/script/controlPersonaje.cs
@@ -7,12 +7,6 @@ public class controlPersonaje : MonoBehaviour
 
     public float velocidadMovimiento;
     public Rigidbody2D elRB;
-    private bool dobleSalto;
-    public float fuerzaSalto;
-
-    private bool esElPiso;
-    public Transform puntoDetectaPiso;
-    public LayerMask esPiso;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,28 +16,6 @@ public class controlPersonaje : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        elRB.velocity = new Vector2( velocidadMovimiento* Input.GetAxisRaw ("Horizontal"), elRB.velocity.y );
-    
-        esElPiso = Physics2D.OverlapCircle(puntoDetectaPiso.position,2f,esPiso);
-
-      if(esElPiso)
-        {
-           dobleSalto = true;
-        }
-
-        if (Input.GetButtonDown("Jump"))
-        {
-            if(esElPiso)
-            {
-             elRB.velocity = new Vector2(elRB.velocity.x, fuerzaSalto);
-            } else 
-            {
-                if(dobleSalto)
-                {
-                  elRB.velocity = new Vector2(elRB.velocity.x, fuerzaSalto);  
-                  dobleSalto = false;
-                }
-            }
-        }
+        elRB.velocity = new Vector2( velocidadMovimiento* Input.GetAxis ("Horizontal"), elRB.velocity.y );
     }
 }
