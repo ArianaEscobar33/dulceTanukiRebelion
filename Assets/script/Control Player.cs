8e2cad3fa9a25fa4572eb5b9ab559f95d4baae1b
JoniGF <jonikpoooooo@gmail.com>
2023-10-07 18:00:40 -0300

    Movimiento y Animacion
    Modificación del movimiento, Flipeo, Saltos, Saltos dobles
    Animación de descanso, de correr y saltar

---
+++ Assets/script/Control Player.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlPlayer : MonoBehaviour
+{
+    [Header("Movimiento")]
+    public float velMov;
+    public float altSalto;
+    private bool sePuedeDobleSalto;
+
+    [Header("Componentes")]
+    public Rigidbody2D cuerpoRigido;
+    private bool esSuelo;
+    public Transform controlSuelo;
+    public LayerMask queEsSuelo;
+
+    [Header("Animacion")]
+    private Animator anim;
+    private SpriteRenderer spriteR;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        spriteR = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // movimiento lineal
+        cuerpoRigido.velocity = new Vector2(velMov * Input.GetAxis("Horizontal"),cuerpoRigido.velocity.y);
+
+        // Saltos
+        esSuelo = Physics2D.OverlapCircle(controlSuelo.position, .2f, queEsSuelo);
+        if (esSuelo)
+        {
+            sePuedeDobleSalto = true;
+        }
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (esSuelo)
+            {
+                cuerpoRigido.velocity = new Vector2(cuerpoRigido.velocity.x,altSalto);
+            }else{
+                if (sePuedeDobleSalto)
+                {
+                    cuerpoRigido.velocity = new Vector2(cuerpoRigido.velocity.x,altSalto);
+                    sePuedeDobleSalto = false;
+                }
+            }
+        }
+
+        if (cuerpoRigido.velocity.x <0)
+        {
+            spriteR.flipX=false;
+        }else if (cuerpoRigido.velocity.x >0)
+        {
+            spriteR.flipX=true;
+        }
+
+        //Animacion
+        anim.SetFloat("velMov",Mathf.Abs(cuerpoRigido.velocity.x));
+        anim.SetBool("esSuelo",esSuelo);
+    }
+}
