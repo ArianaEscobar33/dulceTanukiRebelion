9ba37710f5eedb314449a0a487b9a04b05b1c808
JoniGF <jonikpoooooo@gmail.com>
2023-10-08 00:38:08 -0300

    Cambios Escenario

--- Assets/script/Camera Control.cs
+++ Assets/script/Camera Control.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CameraControl : MonoBehaviour
 {
     public Transform target;
-    public Transform fondoAtras, fondoMedio, fondoAdelante,fondo, suelo;
+    public Transform fondoAtras, fondoMedio, fondoAdelante,fondo;
     private Vector2 ultPos;
     public float minAlt, maxAlt;
 
@@ -19,10 +19,10 @@ public class CameraControl : MonoBehaviour
         transform.position = new Vector3(target.position.x, Mathf.Clamp(target.position.y, minAlt, maxAlt), transform.position.z);
         Vector2 cantMovCam = new Vector2(transform.position.x - ultPos.x,transform.position.y - ultPos.y);
         fondo.position = fondo.position + new Vector3(cantMovCam.x, cantMovCam.y, 0f);
-        fondoAtras.position += new Vector3(cantMovCam.x, cantMovCam.y, 0f)*0.95f;
-        fondoMedio.position += new Vector3(cantMovCam.x, cantMovCam.y, 0f)*0.9f;
-        fondoAdelante.position += new Vector3(cantMovCam.x, cantMovCam.y, 0f)*1.05f;  
-        suelo.position += new Vector3(cantMovCam.x, 0f, 0f);
+        fondoAtras.position += new Vector3(cantMovCam.x, cantMovCam.y, 0f)*1.1f;
+        fondoMedio.position += new Vector3(cantMovCam.x, cantMovCam.y, 0f)*1f;
+        fondoAdelante.position += new Vector3(cantMovCam.x, cantMovCam.y, 0f)*1.25f;  
+        //suelo.position += new Vector3(cantMovCam.x, 0f, 0f);
         ultPos = transform.position;
     }
 }
